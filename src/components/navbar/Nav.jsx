cf806ae2805a36ef2021a8edaf83e629fd2f2ccb
mustafa mehel <mehelx5207@gmail.com>
2022-11-01 08:42:38 +0300

    hamb

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
-import NavStyled, { NavbarLink, NavImg, NavbarLinka } from "./NavStyled.js";
+import NavStyled, {
+  NavbarLink,
+  NavImg,
+  NavbarLinka,
+  HamburgerMenu,
+  MenuDiv,
+} from "./NavStyled.js";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   return (
     <NavStyled>
@@ -14,8 +23,10 @@ function Nav() {
         />
         <NavbarLink to="/home">Recipe</NavbarLink>
       </div>
-
-      <div>
+      <HamburgerMenu onClick={() => setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </HamburgerMenu>
+      <MenuDiv isOpen={isOpen} onClick={() => setIsOpen(false)}>
         <NavbarLink to="/about">About</NavbarLink>
 
         <NavbarLinka href="https://github.com/mmehel52" target="_blank">
@@ -23,7 +34,7 @@ function Nav() {
         </NavbarLinka>
 
         <NavbarLink to="/">Logout</NavbarLink>
-      </div>
+      </MenuDiv>
     </NavStyled>
   );
 }
--- src/components/navbar/NavStyled.js
+++ src/components/navbar/NavStyled.js
@@ -5,8 +5,8 @@ const NavStyled = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  flex-wrap: wrap;
   @media (max-width: ${({ theme }) => theme.responsive}) {
-    flex-direction: column;
   }
 `;
 export const NavbarLink = styled(Link)`
@@ -14,17 +14,53 @@ export const NavbarLink = styled(Link)`
   color: ${({ theme }) => theme.colors.text};
   font-size: 1.5rem;
   margin: 0 1rem;
+  &:hover {
+    color: ${({ theme }) => theme.colors.body};
+    font-weight: bold;
+  }
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    width: 100%;
+
+    text-align: center;
+  }
 `;
 export const NavbarLinka = styled.a`
   text-decoration: none;
   color: ${({ theme }) => theme.colors.text};
   font-size: 1.5rem;
   margin: 0 1rem;
+  &:hover {
+    color: ${({ theme }) => theme.colors.body};
+    font-weight: bold;
+  }
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    width: 100%;
+
+    text-align: center;
+  }
 `;
 export const NavImg = styled.img`
   width: 8rem;
   border-radius: 50%;
   padding: 1rem;
 `;
-
+export const HamburgerMenu = styled.div`
+  display: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.text};
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    display: block;
+  }
+`;
+export const MenuDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    flex-direction: column;
+    width: 100%;
+    max-height: ${({ isOpen }) => (isOpen ? "400px" : "0")};
+    overflow: hidden;
+  }
+`;
 export default NavStyled;
